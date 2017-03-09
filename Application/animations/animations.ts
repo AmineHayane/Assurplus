34f515ec21cd782c8330eefa216c257db0b1e6e4
Bob <b.kouhen@gmail.com>
2017-03-09 15:49:19 +0100

    Animations V1
    Add animation files

---
+++ Application/animations/animations.ts
@@ -0,0 +1,11 @@
+import { enterLeaveLeft, enterLeaveRight} from './enterleave.animation';
+import { scale } from './scale.animation';
+import { flyInOrOut } from './flyInOrOut';
+
+export class Animations {
+  public static enterLeaveLeft = enterLeaveLeft;
+  public static enterLeaveRight = enterLeaveRight;
+  public static scale = scale;
+  public static flyInOrOut = flyInOrOut;
+}
+
